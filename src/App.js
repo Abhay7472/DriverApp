06b78d6ff81bd9e2aab2981b74892f37a6913f26
devendra sharma <devendra.hos7@gmail.com>
2021-09-15 15:56:36 +0530

    notification issue fixes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@
  * @flow
  */
 import 'react-native-gesture-handler';
-import React, { useEffect,createRef } from 'react';
-import { View, Dimensions,YellowBox,LogBox } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DrawerContent } from './navigation/DrawerContent';
+import React, {useEffect, createRef} from 'react';
+import {View, Dimensions, YellowBox, LogBox} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {DrawerContent} from './navigation/DrawerContent';
 import MainTabScreen from './navigation/MainTabScreen';
 import RootStackScreen from './screens/loginScreens/RootStackScreen';
 import * as Animatable from 'react-native-animatable';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import images from './images';
 import {setuser} from './constants/tokenHandler';
-import NotifiStackScreen from './screens/Notifi&HelpStack/NotifiStackScreen'
+import NotifiStackScreen from './screens/Notifi&HelpStack/NotifiStackScreen';
 import {setnotifiToken} from './constants/tokenHandler';
 import SelfiUpload from './screens/Notifi&HelpStack/SelfiUpload';
 import messaging from '@react-native-firebase/messaging';
@@ -26,39 +26,38 @@ import {fcmService} from './FCMService';
 import {localNotificationService} from './localNotification';
 import {firebase} from '@react-native-firebase/messaging';
 
-import { AuthContext } from './components/context';
-import { Permission, PERMISSIONS_TYPE } from './constants/Permission';
+import {AuthContext} from './components/context';
+import {Permission, PERMISSIONS_TYPE} from './constants/Permission';
 const Drawer = createDrawerNavigator();
 
-const App = (props,{navigation}) => {
+const App = (props, {navigation}) => {
   const [isLoading, setIsLoading] = React.useState(true);
 
   const initialLoginState = {
     userToken: null,
   };
 
-
   const loginReducer = (prevState, action) => {
-    switch( action.type ) {
-      case 'RETRIEVE_TOKEN': 
-        return { 
-          ...prevState,
-          userToken: action.token,
-          isLoading: false,
-        };
-      case 'LOGIN': 
+    switch (action.type) {
+      case 'RETRIEVE_TOKEN':
         return {
           ...prevState,
           userToken: action.token,
           isLoading: false,
         };
-      case 'LOGOUT': 
+      case 'LOGIN':
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGOUT':
         return {
           ...prevState,
           userToken: null,
           isLoading: false,
         };
-      case 'REGISTER': 
+      case 'REGISTER':
         return {
           ...prevState,
           userToken: action.token,
@@ -67,36 +66,38 @@ const App = (props,{navigation}) => {
     }
   };
 
-  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+  const [loginState, dispatch] = React.useReducer(
+    loginReducer,
+    initialLoginState,
+  );
 
-  const authContext = React.useMemo(() => ({
-    signIn: async(token) => {
-      // setUserToken('fgkj');
-      // setIsLoading(false);
-      const userToken = String(token);
-      
-      try {
-        await AsyncStorage.setItem('userToken', userToken);
-      } catch(e) {
-        console.log('appjs signIn',e);
-      }
-      dispatch({ type: 'LOGIN', token: userToken });
-      
-    },
-     signOut: async() => {
-      // setUserToken(null);
-      // setIsLoading(false);
-      try {
-        await AsyncStorage.removeItem('userToken');
-      } catch(e) {
-        console.log(e);
-      }
-      dispatch({ type: 'LOGOUT' });
-    },
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async token => {
+        // setUserToken('fgkj');
+        // setIsLoading(false);
+        const userToken = String(token);
 
-    
-  }), []);
-
+        try {
+          await AsyncStorage.setItem('userToken', userToken);
+        } catch (e) {
+          console.log('appjs signIn', e);
+        }
+        dispatch({type: 'LOGIN', token: userToken});
+      },
+      signOut: async () => {
+        // setUserToken(null);
+        // setIsLoading(false);
+        try {
+          await AsyncStorage.removeItem('userToken');
+        } catch (e) {
+          console.log(e);
+        }
+        dispatch({type: 'LOGOUT'});
+      },
+    }),
+    [],
+  );
 
   const handleFcmToken = () => {
     if (!firebase.apps.length) {
@@ -108,7 +109,7 @@ const App = (props,{navigation}) => {
 
     function onRegister(token) {
       // setFcmToken(token);
-      setnotifiToken(token)
+      setnotifiToken(token);
     }
 
     function onNotification(notify) {
@@ -116,7 +117,7 @@ const App = (props,{navigation}) => {
       const options = {
         soundName: 'default',
         playSound: true, //,
-        foreground:true
+        foreground: true,
       };
       localNotificationService.showNotification(
         0,
@@ -129,81 +130,87 @@ const App = (props,{navigation}) => {
 
     function onOpenNotification(notify) {
       console.log('[App]: ', notify);
-      var notificationDetails =JSON.parse(notify?.notification_detail);
-      navigationRef.current.navigate('NotifiStackScreen',{screen: 'SelfiUpload',params:notificationDetails})
-      
+      if (notify && notify?.notification_detail) {
+        var notificationDetails = JSON.parse(notify?.notification_detail);
+        navigationRef.current.navigate('NotifiStackScreen', {
+          screen: 'SelfiUpload',
+          params: notificationDetails ? notificationDetails : '',
+        });
+      }
     }
-    
   };
-  useEffect(()=>{
+  useEffect(() => {
     Permission.requestMultiple([
       PERMISSIONS_TYPE.photo,
       PERMISSIONS_TYPE.camera,
     ]);
-   
-  },[])
+  }, []);
   useEffect(() => {
     handleFcmToken();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    setTimeout(async() => {
+    setTimeout(async () => {
       let userToken;
       userToken = null;
       try {
         userToken = await AsyncStorage.getItem('userToken');
-         setuser(userToken);
-      } catch(e) {
+        setuser(userToken);
+      } catch (e) {
         console.log(e);
       }
-      dispatch({ type: 'RETRIEVE_TOKEN', token: userToken });
+      dispatch({type: 'RETRIEVE_TOKEN', token: userToken});
     }, 3000);
   }, []);
-  
-  
-  if( loginState.isLoading ) {
-    return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor: '#000'}}>
-        <Animatable.Image 
-            animation="bounceIn"
-            duraton="1000"
-            source={images.logo}
-            style={{width: height_logo,height: height_logo}}
-            resizeMode="stretch"
-        /> 
+
+  if (loginState.isLoading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#000',
+        }}>
+        <Animatable.Image
+          animation="bounceIn"
+          duraton="1000"
+          source={images.logo}
+          style={{width: height_logo, height: height_logo}}
+          resizeMode="stretch"
+        />
       </View>
     );
   }
 
-    console.reportErrorsAsExceptions = false;
-      LogBox.ignoreLogs([
-        'Require cycle:'
-      ])
+  console.reportErrorsAsExceptions = false;
+  LogBox.ignoreLogs(['Require cycle:']);
 
-    //  YellowBox.ignoreWarnings([
-    //     'Require cycle:'
-    //   ])
+  //  YellowBox.ignoreWarnings([
+  //     'Require cycle:'
+  //   ])
 
   return (
     <AuthContext.Provider value={authContext}>
-    <NavigationContainer ref={navigationRef}>
-      { loginState.userToken !== null ? (
-     <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
-          <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
-          <Drawer.Screen name="NotifiStackScreen" component={NotifiStackScreen} />
-      </Drawer.Navigator>
-      )
-    :
-      <RootStackScreen/>
-    }
-    </NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
+        {loginState.userToken !== null ? (
+          <Drawer.Navigator
+            drawerContent={props => <DrawerContent {...props} />}>
+            <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+            <Drawer.Screen
+              name="NotifiStackScreen"
+              component={NotifiStackScreen}
+            />
+          </Drawer.Navigator>
+        ) : (
+          <RootStackScreen />
+        )}
+      </NavigationContainer>
     </AuthContext.Provider>
   );
-}
+};
 
 export default App;
 
-const {height} = Dimensions.get("screen");
+const {height} = Dimensions.get('screen');
 const height_logo = height * 0.28;
-
-
